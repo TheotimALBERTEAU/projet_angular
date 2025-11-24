c2875beaa2c04f2eebe7de2028ae223a77fc1985
Théotim ALBERTEAU <sunshouwu@outlook.com>
2025-11-24 23:16:30 +0100

    Remove Api

--- ApiArticle/core/http-library.js
+++
@@ -1,8 +0,0 @@
-const { buildAPIResponse } = require('./helpers-library');
-
-module.exports = {
-
-    httpApiResponse : (response, code, message, data) => {
-        return response.json(buildAPIResponse(code, message, data));
-    },
-}
